eludris-community/eludris_flutter

commit e81ba19466f14630eee2a51d8d30c1b8aee33629

feat: implement eludris v0.3 changes (#39)

lib/api/gateway.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:eludris/api/message.dart';
 import 'package:websocket_universal/websocket_universal.dart';
+
+class EludrisProcessor extends SocketSimpleTextProcessor {
+  @override
+  get pingServerMessage => jsonEncode({"op": "PING"});
+}
 
 class Gateway {
   late final String url;
@@ -11,18 +17,21 @@ class Gateway {
   final onMessageCreate = StreamController<Message>.broadcast();
 
   var connectionOptions = const SocketConnectionOptions(
-    pingIntervalMs: 20 * 1000,
+    pingIntervalMs: 45 * 1000,
   );
 
   Gateway({required this.url});
 
   void _handler(String data) {
-    onMessageCreate.add(Message.fromJson(data));
+    final event = jsonDecode(data);
+    if (event["op"] == "MESSAGE_CREATE") {
+      onMessageCreate.add(Message.fromMap(event["d"]));
+    }
   }
 
   Future<void> connect() async {
     final IMessageProcessor<String, String> textSocketProcessor =
-        SocketSimpleTextProcessor();
+        EludrisProcessor();
     _ws = IWebSocketHandler<String, String>.createClient(
       url,
       textSocketProcessor,

lib/api/http.dart
@@ -12,7 +12,7 @@ class HTTP {
     final request = Request('POST', Uri.parse('$baseUrl/messages'));
     request.body = jsonEncode({'author': author, 'content': content});
 
-    final response = await request.send();
-    return Message.fromJson(await response.stream.bytesToString());
+    await request.send();
+    return Message(author, content, true);
   }
 }

lib/api/message.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class Message {
   late String author;
   late bool optimistic;
@@ -8,10 +6,9 @@ class Message {
 
   Message(this.author, this.content, this.optimistic, {this.plugin});
 
-  Message.fromJson(String json) {
-    final Map<String, dynamic> map = jsonDecode(json);
-    author = map['author'];
-    content = map['content'];
+  Message.fromMap(Map<String, String> data) {
+    author = data['author']!;
+    content = data['content']!;
     optimistic = false;
     plugin = null;
   }

commit 361490c98439e9cbb2b5a1e3a7995b3008ebb69e

fix: use Map.from for gateway data Error was introduced in #39

lib/api/gateway.dart
@@ -25,7 +25,7 @@ class Gateway {
   void _handler(String data) {
     final event = jsonDecode(data);
     if (event["op"] == "MESSAGE_CREATE") {
-      onMessageCreate.add(Message.fromMap(event["d"]));
+      onMessageCreate.add(Message.fromMap(Map.from(event["d"])));
     }
   }
 